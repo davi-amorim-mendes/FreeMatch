3642d80fa186ff41476f827f4f0d35fd8401a81f
davi-amorim-mendes <davi.amorim.mendes.082@gmail.com>
2026-01-27 15:57:40 -0300

    add: Alteração na conexão do banco de dados para o render

--- model/database.py
+++ model/database.py
@@ -1,19 +1,17 @@
 import os
 import psycopg2
 from urllib.parse import urlparse
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 url = urlparse(DATABASE_URL)
 class SQL:
     @staticmethod
     def conexao():
         return psycopg2.connect(
-            host=url.path[1:],
+            host=url.hostname,
             user=url.username,
             password=url.password,
-            database=url.hostname,
+            database=url.path[1:],
             port=url.port
         )
